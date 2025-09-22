6fd7f0928fcf5c1da90ce10fdf61978c903bb3d4
vivek <vivek_msn@hotmail.com>
2025-09-22 17:07:33 +0530

    new code

--- wp-content/themes/personalportfolio/category.php
+++ wp-content/themes/personalportfolio/category.php
@@ -6,28 +6,35 @@
             <!-- Category Title & Description -->
             <div class="col-12 text-center mb-5">
                 <?php 
-$category = get_queried_object();
-$cat_image = get_term_meta($category->term_id, 'category_image', true);
-?>
+                $category = get_queried_object();
+                $cat_image = get_term_meta($category->term_id, 'category_image', true);
+                ?>
 
                 <h2><?php single_cat_title(); ?></h2>
                 <p><?php echo category_description(); ?></p>
-            </div>
 
+                <!-- Show Category Image (only once) -->
+                <?php if ($cat_image): ?>
+                    <div class="mt-4">
+                        <img src="<?php echo esc_url($cat_image); ?>" 
+                             alt="<?php echo esc_attr($category->name); ?>" 
+                             class="img-fluid">
+                    </div>
+                <?php endif; ?>
+            </div>
 
             <!-- Posts Loop -->
             <?php if(have_posts()) : ?>
                 <?php while(have_posts()) : the_post(); ?>
                     <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
                         <div class="card rounded-0 border-0 p-0">
+                            <!-- Post Thumbnail -->
                             <?php if(has_post_thumbnail()): ?>
                                 <?php the_post_thumbnail('full', ['class'=>'img-fluid']); ?>
                             <?php else: ?>
-                                <?php if ($cat_image): ?>
-    <div class="col-12 text-center mb-4">
-        <img src="<?php echo esc_url($cat_image); ?>" alt="<?php echo esc_attr($category->name); ?>" class="img-fluid">
-    </div>
-<?php endif; ?>
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/blog/blog.webp" 
+                                     alt="blog" 
+                                     class="img-fluid">
                             <?php endif; ?>
 
                             <div class="blog_details text-center">
