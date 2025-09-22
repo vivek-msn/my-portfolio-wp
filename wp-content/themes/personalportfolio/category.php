b88365b3c3c2a4bb0bb0653e3be5c957a9d43510
vivek <vivek_msn@hotmail.com>
2025-09-22 17:03:50 +0530

    add more

--- wp-content/themes/personalportfolio/category.php
+++ wp-content/themes/personalportfolio/category.php
@@ -13,11 +13,7 @@ $cat_image = get_term_meta($category->term_id, 'category_image', true);
                 <h2><?php single_cat_title(); ?></h2>
                 <p><?php echo category_description(); ?></p>
             </div>
-<?php if ($cat_image): ?>
-    <div class="col-12 text-center mb-4">
-        <img src="<?php echo esc_url($cat_image); ?>" alt="<?php echo esc_attr($category->name); ?>" class="img-fluid">
-    </div>
-<?php endif; ?>
+
 
             <!-- Posts Loop -->
             <?php if(have_posts()) : ?>
@@ -27,7 +23,11 @@ $cat_image = get_term_meta($category->term_id, 'category_image', true);
                             <?php if(has_post_thumbnail()): ?>
                                 <?php the_post_thumbnail('full', ['class'=>'img-fluid']); ?>
                             <?php else: ?>
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/blog/blog.webp" alt="blog" class="img-fluid">
+                                <?php if ($cat_image): ?>
+    <div class="col-12 text-center mb-4">
+        <img src="<?php echo esc_url($cat_image); ?>" alt="<?php echo esc_attr($category->name); ?>" class="img-fluid">
+    </div>
+<?php endif; ?>
                             <?php endif; ?>
 
                             <div class="blog_details text-center">
